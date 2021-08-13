df6c8b060a61825d150cfdce59fbe65e0c3fba98
Antonio Toriz <torizantonio15@gmail.com>
2021-08-13 16:53:25 -0700

    created new XCode project, did literally nothing else 🤷

---
+++ mewsk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mewsk
+//
+//  Created by Antonio Toriz on 8/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
