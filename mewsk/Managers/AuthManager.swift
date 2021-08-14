ae2128933cf0166a8cd9056b7bfaf18810218a26
Antonio Toriz <torizantonio15@gmail.com>
2021-08-14 12:27:40 -0700

    added signin with Spotify functionality, not done yet. redirects to www.antoniotoriz.com for now

--- mewsk/Managers/AuthManager.swift
+++ mewsk/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "4d8042112fcf4d9b93d76a6e70568eba"
+        static let clientSecret = "cdf5f149325a46f9bda247e44ffcc143"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let baseURL = "https://accounts.spotify.com/authorize"
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.antoniotoriz.com"
+        let URLString = "\(baseURL)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: URLString)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -32,4 +45,18 @@ final class AuthManager {
         return false
     }
     
+    public func exchangeCodeForToken(code: String, completion: ((Bool) -> Void)) {
+        // get token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    public func cacheToken() {
+        
+    }
+    
+    
+        
 }
